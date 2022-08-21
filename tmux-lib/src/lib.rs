e5ba5d7b3e30aca754bc25bceef93307c49a1766
graelo <graelo@grael.cc>
2022-08-21 09:59:34 +0200

    fix(license): consistent files & statement

--- tmux-lib/src/lib.rs
+++ tmux-lib/src/lib.rs
@@ -1,4 +1,34 @@
 //! Functions to read or manipulate Tmux
+//!
+//! Version requirement: _rustc 1.50+_
+//!
+//! ```toml
+//! [dependencies]
+//! tmux-backup = "0.1"
+//! ```
+//!
+//! ## Getting started
+//!
+//! Work in progress
+//!
+//! ## Caveats
+//!
+//! - This is a beta version
+//!
+//! ## License
+//!
+//! Licensed under either of
+//!
+//! - [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+//! - [MIT license](http://opensource.org/licenses/MIT)
+//!
+//! at your option.
+//!
+//! ### Contribution
+//!
+//! Unless you explicitly state otherwise, any contribution intentionally submitted
+//! for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+//! be dual licensed as above, without any additional terms or conditions.
 
 pub mod error;
 
